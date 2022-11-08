d42c8721f309615c676c1d9c072a8ff643730862
robin.chenyu <robin.chenyu@gmail.com>
2022-11-08 20:30:38 +0800

    sync

---
+++ export.sh
@@ -0,0 +1,6 @@
+logseq-export \
+  --graphPath /home/robin/ls-repo \
+  --blogFolder /home/robin/notes/private/blog \
+  --unquotedProperties date,slug,public,tags \
+  --assetsRelativePath static/images/logseq \
+  --webAssetsPathPrefix /images/logseq
